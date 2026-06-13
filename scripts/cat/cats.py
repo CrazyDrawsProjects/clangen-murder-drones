6129b9176a57349d201475de51f153ec98414d73
CrazythecatDraws <114513991+CrazythecatDraws@users.noreply.github.com>
2026-06-13 18:31:51 -0400

    add species weights & hybrids code

--- scripts/cat/cats.py
+++ scripts/cat/cats.py
@@ -529,6 +529,16 @@ class Cat:
             if par2species:
                 par_species.append(par2species)
 
+            if par_species == ['worker drone', 'disassembly drone']:
+                self.species = "worker x disassembly drone hybrid"
+            else:
+                self.species = choices(species_list, weights=(1, 1, 1, 0, 0), k=1)[0]
+
+            if par_species == ['solver infected worker drone', 'disassembly drone']:
+                self.species = "solver infected worker x disassembly drone hybrid"
+            else:
+                self.species = choices(species_list, weights=(1, 1, 1, 0, 0), k=1)[0]
+
             if not par_species:
                 print("[SPS] Warning - par_species none: species randomized")
                 self.species = choices(species_list, weights=weights, k=1)[0]
